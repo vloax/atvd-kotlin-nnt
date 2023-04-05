05823c1843831225dd67b712b1821afb07a39875
vloax <vitords.mello@gmail.com>
2023-04-05 16:43:29 -0400

    First part of all

---
+++ atividades.kt
@@ -0,0 +1,20 @@
+//Diferencas de um arquivo Kotlin para um arquivo Java.
+
+fun main() {
+  /* a -> Crie uma variável do tipo inteiro e atribua o valor 10. Em seguida, crie uma constante e
+  atribua o valor 5. Qual a diferença entre variáveis e constantes? */
+
+  var numero1: Int = 10;
+  val numero2: Int = 5;
+
+  //Variáveis podem ser alteradas durante o código, já constantes tem um valor constante não podendo ser alteradas.
+
+  /* a -> Crie uma variável para armazenar um número decimal. Qual tipo de dado você deve
+  utilizar? R= Deve ser usada DOUBLE ou FLOAT */
+
+  var numero3: Double = 10.540;
+
+  
+
+
+}
